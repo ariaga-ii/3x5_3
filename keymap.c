fd5758b045c4c366fd49c0a4d7a5e3fee396b779
Adam Juraszek <adam@Adams-MacBook-Pro-2.local>
2022-01-27 20:34:56 +0000

    test commit

--- keymap.c
+++ keymap.c
@@ -14,7 +14,7 @@ static uint32_t oled_timer = 0;
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
-//   entirely and just use numbers.   
+// entirely and just use numbers.   
 enum layers {
   _QWERTY,
   _LOWER,
