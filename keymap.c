302b6db14325efc0e710567c066102a7453cd7b2
Adam Juraszek <adam@Adams-MacBook-Pro-2.local>
2022-01-27 20:31:04 +0000

    symbol layer

--- keymap.c
+++ keymap.c
@@ -61,6 +61,7 @@ enum custom_keycodes {
 
 
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_split_3x5_3(
          KC_Q,  KC_W,  KC_E,     KC_R,   KC_T,                                          KC_Y,           KC_U,      KC_I,      KC_O,       KC_P, 
